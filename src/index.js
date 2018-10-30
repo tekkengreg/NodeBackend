3fa4b3b0eb9b3713a4ed3a1c98339fa645c44d7f
Gregory <grpetit@gmail.com>
2018-10-30 14:13:47 +0100

    bigbang

--- src/index.js
+++ src/index.js
@@ -10,7 +10,12 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
 
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+  next();
+});
 
 app.use('/', require('./routes'));
 
