707f58383aed91b555d434cff2d652bf25927dac
Gregory Petit <grpetit@gmail.com>
2019-06-24 17:45:08 +0200

    ajout jwt

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const JWTStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt
+const jwt = require('jsonwebtoken');
 
 const app = express();
 
-app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
@@ -17,7 +20,54 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/', require('./routes'));
+app.use((req, res, next) => {
+  // console.log(JSON.stringify(req.headers));
+  console.log(req.headers.authorization.split(' ')[1])
+    next()
+})
+
+app.use(passport.initialize());
+
+const userFake = {
+  email: 'wild@wild.fr',
+  password: 'wilder'
+}
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+},
+  function (email, password, done) {
+    console.log('local', email, password)
+    if (email === userFake.email
+      && password === userFake.password)
+      done(null, { email: userFake.email })
+    else
+      done(null, false)
+  }
+));
+
+passport.use(new JWTStrategy({
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: 'mon_secret'
+},
+  function (payload, done) {
+    if (payload) done(null, payload)
+    else done(null, false)
+  })
+)
+
+app.post('/auth/signin', passport.authenticate('local', { session: false }), 
+(req, res) => {
+  console.log("signin", req.user)
+  res.json(jwt.sign(req.user, 'mon_secret'))
+})
+
+app.get('/hellome', passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    res.send('hello ' + req.user.email)
+  }
+)
+
+//app.use('/', require('./routes'));
 
 
 
